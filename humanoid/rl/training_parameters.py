58c95be0e0cfe2198f07274529938aca1a2f57f7
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-02-27 00:03:46 -0500

    comment change

--- humanoid/rl/training_parameters.py
+++ humanoid/rl/training_parameters.py
@@ -9,8 +9,6 @@ os.environ["RENDER_SIM"] = "False"
 
 # STATE INFO FROM https://arxiv.org/pdf/2304.13653.pdf
 
-# HYPERPARAMETER INFO FROM https://arxiv.org/pdf/1910.10620.pdf#page=3&zoom=100,433,952
-
 # STATE
     # joint positions     5 · 20          Joint positions in radians (stacked last 5 timesteps)
     # linear acceleration 5 · 3           Linear acceleration from IMU (stacked)
@@ -60,6 +58,7 @@ state_dim = (env.observation_shape[1] + env.action_shape[1]) * state_history_len
 action_dim = env.action_shape[1]
 #####################################################
 
+# HYPERPARAMETER INFO FROM https://arxiv.org/pdf/1910.10620.pdf#page=3&zoom=100,433,952
 ################ PPO hyperparameters ################
 update_timesteps = int(4096*64 / env.count)      # update policy every n timesteps
 K_epochs = 10               # update policy for K epochs in one PPO update
