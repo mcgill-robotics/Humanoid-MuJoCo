2ec111809d7ec84d9635b76391bd206e7cc755d6
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-03-16 23:50:27 -0400

    update for fixes to GPU hyperparam optimization

--- simulation/hyperparam_config.py
+++ simulation/hyperparam_config.py
@@ -3,7 +3,7 @@ import torch
 from stable_baselines3.common.torch_layers import FlattenExtractor
 
 # TAKEN FROM RL ZOO HYPERPARAMS FOR Humanoid-v4 mujoco environment
-default_hyperparams = dict(
+cpu_hyperparams = dict(
     policy = 'MlpPolicy',
     normalize = True,
     n_timesteps = 1e7,
@@ -26,8 +26,30 @@ default_hyperparams = dict(
                     )
     )
 
+gpu_hyperparams = dict(
+    policy = 'MlpPolicy',
+    normalize = True,
+    n_timesteps = 1e7,
+    n_envs = 128,
+    batch_size = 256,
+    n_steps = 512,
+    gamma = 0.95,
+    learning_rate = 3.56987e-05,
+    ent_coef = 0.00238306,
+    clip_range = 0.3,
+    n_epochs = 5,
+    gae_lambda = 0.9,
+    max_grad_norm = 2,
+    vf_coef = 0.431892,
+    policy_kwargs = dict(
+                        log_std_init = -2,
+                        ortho_init = False,
+                        activation_fn = nn.ReLU,
+                        net_arch = dict(pi=[256, 256], vf=[256, 256])
+                    )
+    )
 
 hyperparams = {
-    "CPUHumanoid": default_hyperparams,
-    "GPUHumanoid": default_hyperparams
+    "CPUHumanoid": cpu_hyperparams,
+    "GPUHumanoid": gpu_hyperparams
 }
