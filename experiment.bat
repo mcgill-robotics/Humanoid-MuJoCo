8b7829ee36231d0dc5db7c59c581aeb147c9aae8
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-04-17 12:21:37 -0400

    experiment with randomization 1.0 failed to learn: trying new setup (increase randomness of initial joint angles in increments of 0.1 rad)

--- experiment.bat
+++ experiment.bat
@@ -1,3 +1,3 @@
-python3 train.py --algo sac --cpu --log-name SAC --rand-init 1.0
-python3 train.py --algo ppo --cpu --log-name PPO --rand-init 1.0
-python3 train.py --algo td3 --cpu --log-name TD3 --rand-init 1.0
+python3 train.py --algo sac --cpu --log-name SAC --rand-init 0.0 --rand-increment 0.2
+python3 train.py --algo ppo --cpu --log-name PPO --rand-init 0.0 --rand-increment 0.2
+@REM python3 train.py --algo td3 --cpu --log-name TD3 --rand-init 0.0
