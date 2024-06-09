a7671a4398108578974b0e3db4e6285dbdc08fe6
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-06-09 23:28:27 +0000

    fix scrit

--- start.sh
+++ start.sh
@@ -10,31 +10,31 @@ execute_test() {
     echo " >> command: $string"
 
     mv $1 $1.in_progress
-    git add $1.in_progress
-    git commit -m "Running $1"  > /dev/null
-    git push > /dev/null
+    git add $1.in_progress --quiet
+    git commit -m "Running $1" --quiet
+    git push --quiet
 
     # CHECKOUT TO TEST BRANCH AND RUN TEST
-    git checkout $git_branch
-    git reset --hard HEAD > /dev/null
+    git checkout $git_branch --quiet
+    git reset --hard --quiet HEAD
     $string &> "$1.logs"
     
     # PUSH TEST RESULTS TO TEST BRANCH
     git add -A
-    git commit -m "Test results" > /dev/null
-    git push > /dev/null
+    git commit -m --quiet "Test results"
+    git push --quiet
 
     # RETURN TO TRAIN_QUEUE BRANCH TO MOVE TEST STATUS TO DONE AND CONTINUE POLLING
-    git checkout train_queue
-    git reset --hard HEAD
+    git checkout train_queue --quiet
+    git reset --hard --quiet HEAD
     mv $1.in_progress $1.done
-    git add $1.done
-    git commit -m "Finished $1"
-    git push > /dev/null
+    git add --quiet $1.done
+    git commit --quiet -m "Finished $1"
+    git push --quiet
 }
 
 while true; do
-    git pull > /dev/null
+    git pull --quiet
     for file in *.test; do
         if [[ -e $file ]]; then
             echo "Test found. Executing..."
