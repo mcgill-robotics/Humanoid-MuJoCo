8da45d55b9bccfec979d35b54f7886567ef9407f
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-06-09 23:18:02 +0000

    add comments

--- start.sh
+++ start.sh
@@ -14,12 +14,15 @@ execute_test() {
     git commit -m "Running $1"
     git push > /dev/null
 
+    # CHECKOUT TO TEST BRANCH AND RUN TEST
     git checkout $git_branch
     exec "$string"
     
+    # PUSH TEST RESULTS TO TEST BRANCH
     git commit -am "Test results"
     git push > /dev/null
 
+    # RETURN TO TRAIN_QUEUE BRANCH TO MOVE TEST STATUS TO DONE AND CONTINUE POLLING
     git checkout train_queue
     mv $1.in_progress $1.done
     git commit -am "Finished $1"
