716b7b7cbe54c61045a3e295113168c0128497bd
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-07-04 18:46:05 -0400

    update experiment

--- train.py
+++ train.py
@@ -79,8 +79,8 @@ NUM_ENVS = args.n_envs
 SIMULATE_ON_GPU = not args.cpu
 N_EVAL_EPISODES = args.n_eval_episodes
 RANDOMIZATION_FACTOR = args.rand_init
-RAND_FACTOR_INCREMENTS = [0.05] * 16
-TRAINING_STEPS = [2_000_000] * 16
+RAND_FACTOR_INCREMENTS = [0.05] * 18
+TRAINING_STEPS = [2_000_000] * 18
 if abs(RANDOMIZATION_FACTOR + sum(RAND_FACTOR_INCREMENTS) - 1.0) > 0.001:
     print(
         "ERR: Randomization factor increments do not sum to 1.0 ({} + {}).".format(
