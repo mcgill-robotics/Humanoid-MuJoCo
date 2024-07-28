ff62b52476a88ae27b43114fb60958ab682923e1
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-07-27 23:55:20 -0400

    training progress

--- visualize_policy.py
+++ visualize_policy.py
@@ -6,7 +6,7 @@ from stable_baselines3 import SAC
 
 MODEL_TYPE = SAC
 RANDOMIZATION_FACTOR = 0.1
-CKPT = "data\SAC_CPU/ckpt_18600000_steps.zip"
+CKPT = "data\SAC_CPU/ckpt_24400000_steps.zip"
 
 # env = GPUVecEnv(
 #     num_envs=1,
