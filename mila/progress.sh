e78c821b08e983519e84bc07dfe8ca877a4950aa
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-09-03 10:44:00 -0400

    remove progress script

--- mila/progress.sh
+++
@@ -1,6 +0,0 @@
-# check if the progress file exists
-if [ ! -f data/SAC/train.progress ]; then
-    echo "No progress file data/SAC/train.progress yet."
-fi
-
-watch -n 0.1 cat data/SAC/train.progress
