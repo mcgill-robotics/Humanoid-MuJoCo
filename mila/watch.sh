97fecee0375dbf3fc7fe0be5620ec2dfb95b62d8
antoinedang <antoine.dangeard@gmail.com>
2024-09-03 10:45:34 -0400

    qol

--- mila/watch.sh
+++ mila/watch.sh
@@ -1,1 +1,1 @@
-watch -n 0.1 "echo =======OUT======= && tail mila/out.txt && echo '' && echo =======ERROR======= && tail mila/err.txt && echo '' && echo ======PROGRESS====== && cat data/SAC/0_steps/train.progress"
+watch -n 0.1 "echo ======PROGRESS====== && cat data/SAC/0_steps/train.progress && echo '' && echo =======OUT======= && tail mila/out.txt && echo '' && echo =======ERROR======= && tail mila/err.txt"
