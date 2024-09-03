7618bd3507702f21b429cbbed5536cec6e85f0bf
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-09-03 10:03:38 -0400

    remove SIF stuff (works without)

--- mila/build_sif.sh
+++
@@ -1,3 +0,0 @@
-rm build_out.txt build_stderr.txt
-
-sbatch --gres=gpu:1 mila_build_sif > sbatch_build_out.txt
--- mila/track_build.sh
+++
@@ -1,3 +0,0 @@
-job_id=$(cat sbatch_build_out.txt | awk '{print $4}')
-
-watch scontrol show job $job_id
