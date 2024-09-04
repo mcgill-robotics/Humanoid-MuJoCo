3796a8abb59d008fd196897a13c8cf7b1e6bbe30
antoinedang <antoine.dangeard@gmail.com>
2024-09-03 20:12:57 -0400

    tweaks

--- mila/install.sh
+++ mila/install.sh
@@ -1,6 +1,6 @@
 # Install all needed deps
 module load python/3.10
-module load cuda/11.8
+module load cuda/12.1.1
 
 # apt-get update
 # sudo apt-get install libgl1-mesa-glx ffmpeg libsm6 libxext6  -y
@@ -8,24 +8,18 @@ module load cuda/11.8
 python -m venv $HOME/humanoid_env
 source $HOME/humanoid_env/bin/activate
 
-pip3 install --no-cache-dir --upgrade numpy==2.0.0
-pip3 install --no-cache-dir numpy-quaternion
-pip3 install --upgrade "jax[cuda11_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+pip3 install --upgrade pip
+
+pip3 install --upgrade numpy==1.26.4
+pip3 install numpy-quaternion
+pip3 install --upgrade "jax[cuda12]"
 pip3 install mujoco
 pip3 install mujoco_mjx
 pip3 install brax
 pip3 install opencv-python
 pip3 install matplotlib
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-# pip3 install nvidia-cudnn-cu11==8.9.6.50
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 pip3 install stable-baselines3[extra]
 pip3 install tensorflow
 pip3 install rl_zoo3
 pip3 install perlin_noise
-
-# install dependencies for rendering with OpenCV
-# sudo apt-get install -y libglfw3
-# sudo apt-get install -y libglfw3-dev
-
-pip3 install --upgrade "jax[cuda12]"
-
--- mila/train.sh
+++ mila/train.sh
@@ -1,3 +1,3 @@
 rm mila/out.txt mila/err.txt
 
-sbatch --gres=gpu:1 mila/train_script > mila/sbatch_out.txt
+sbatch --gres=gpu:1 mila/script > mila/sbatch_out.txt
