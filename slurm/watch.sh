cda2fa0d55ab3872b7090f71f3625bd6fad7e99d
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-09-06 13:28:31 -0400

    update watch scipt

--- slurm/watch.sh
+++ slurm/watch.sh
@@ -1,1 +1,1 @@
-watch -n 0.1 "echo ======PROGRESS====== && (cat data/SAC/0_steps/train.progress || true) && echo '' && echo =======OUT======= && (tail slurm/out.txt || true) && echo '' && echo =======ERROR======= && tail slurm/err.txt"
+watch -n 0.1 "echo ======PROGRESS====== && (cat data/SAC/0_steps/train.progress || true) && echo '' && echo =======OUT======= && (tail slurm/out.txt || true) && echo '' && echo =======ERROR======= && (tail slurm/err.txt || true) && echo '' && echo =======STATUS======= && scontrol show job $(cat slurm/sbatch_out.txt | awk '{print $4}') | grep JobState"
--- visualize_policy.py
+++ visualize_policy.py
@@ -5,7 +5,7 @@ from stable_baselines3 import SAC
 
 MODEL_TYPE = SAC
 RANDOMIZATION_FACTOR = 0
-CKPT = "data/SAC/0_steps/_499968_steps.zip"
+CKPT = "data/SAC/0_steps/_999936_steps.zip"
 
 env = CPUEnv(
     xml_path=SIM_XML_PATH,
