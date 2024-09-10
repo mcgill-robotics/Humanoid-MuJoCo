ec727b1ad3e402e9635883d8087290307f2e9a86
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-09-10 00:45:42 -0400

    LQR working

--- lqr.py
+++ lqr.py
@@ -14,8 +14,49 @@ scene_option.flags[mujoco.mjtVisFlag.mjVIS_CONTACTFORCE] = False
 scene_option.flags[mujoco.mjtVisFlag.mjVIS_TRANSPARENT] = False
 scene_option.flags[mujoco.mjtVisFlag.mjVIS_JOINT] = False
 
+
+# function to calculate the distance the robot should be offset vertically so it is touching the ground
+# This is a workaround since we do not have full state observations: the assumption being the robot will always be touching the ground
+def find_ideal_distance_to_ground():
+    height_offsets = np.linspace(-0.05, 0.05, 2001)
+    vertical_forces = []
+    for offset in height_offsets:
+        mujoco.mj_resetDataKeyframe(model, data, 1)
+        mujoco.mj_forward(model, data)
+        data.qacc = 0
+        data.qpos[2] += offset
+        mujoco.mj_inverse(model, data)
+        vertical_forces.append(data.qfrc_inverse[2])
+
+    # Find the height-offset at which the vertical force is smallest.
+    idx = np.argmin(np.abs(vertical_forces))
+    best_offset = height_offsets[idx]
+    return best_offset
+
+
+def control(model, data, Q, qpos0, ctrl0):
+    A = np.zeros((2 * model.nv, 2 * model.nv))
+    B = np.zeros((2 * model.nv, model.nu))
+    epsilon = 1e-6
+    flg_centered = True
+    # global A, B, K
+    mujoco.mjd_transitionFD(model, data, epsilon, flg_centered, A, B, None, None)
+    # Solve discrete Riccati equation.
+    P = scipy.linalg.solve_discrete_are(A, B, Q, np.eye(nu))
+    # Compute the feedback gain matrix K.
+    K = np.linalg.inv(np.eye(nu) + B.T @ P @ B) @ B.T @ P @ A
+    # Allocate position difference dq.
+    dq = np.zeros(model.nv)
+    # Get state difference dx.
+    mujoco.mj_differentiatePos(model, dq, 1, qpos0, data.qpos)
+    dx = np.hstack((dq, data.qvel)).T
+    # LQR control law.
+    return ctrl0 - K @ dx
+
+
 if __name__ == "__main__":
     model = mujoco.MjModel.from_xml_path(SIM_XML_PATH)
+    model.opt.timestep = 0.001
     data = mujoco.MjData(model)
     renderer = mujoco.Renderer(model, 720, 1080)
     JOINT_QPOS_IDX = []
@@ -32,39 +73,24 @@ if __name__ == "__main__":
         cv2.waitKey(1)
 
     def torques_to_positions(init_qpos, init_qvel, torques):
-        # mujoco.mj_resetDataKeyframe(model, data, 1)
-        data.qpos = init_qpos
-        data.qvel = init_qvel
-        data.qfrc_applied[6:] = torques
+        data.qacc = 0
         data.qfrc_actuator[6:] = torques
-        for _ in range(10):
-            mujoco.mj_step(model, data)
-            mujoco.mj_forward(model, data)
-            mujoco.mj_inverse(model, data)
-        print(torques)
-        print(data.qfrc_actuator)
-        print(data.actuator_force)
-        print(data.qfrc_applied)
-        print(data.qfrc_inverse)
-        print(init_qpos - data.qpos)
+        mujoco.mj_step(model, data)
         return data.qpos[JOINT_QPOS_IDX]
 
     # CALCULATE CTRL0
     mujoco.mj_resetDataKeyframe(model, data, 1)
     mujoco.mj_forward(model, data)
     data.qacc = 0
+    # data.qpos[2] += find_ideal_distance_to_ground()
     qpos0 = data.qpos.copy()
     mujoco.mj_inverse(model, data)
     qfrc0 = data.qfrc_inverse.copy()
     ctrl0 = np.atleast_2d(qfrc0) @ np.linalg.pinv(data.actuator_moment)
     ctrl0 = ctrl0.flatten()
-    print(ctrl0)
 
     nu = model.nu  # Alias for the number of actuators.
     nv = model.nv  # Shortcut for the number of DoFs.
-
-    # CREATE R MATRIX
-    R = np.eye(nu)
 
     # CALCULATE Q MATRIX
     # Get the Jacobian for the root body (torso) CoM.
@@ -73,86 +99,76 @@ if __name__ == "__main__":
     mujoco.mj_forward(model, data)
     jac_com = np.zeros((3, nv))
     mujoco.mj_jacSubtreeCom(model, data, jac_com, model.body("humanoid_world_link").id)
+
     # Get the Jacobian for the left foot.
     jac_left_foot = np.zeros((3, nv))
     mujoco.mj_jacBodyCom(
         model, data, jac_left_foot, None, model.body("left_knee_pitch_link").id
     )
+    jac_diff_left = jac_com - jac_left_foot
+    Qbalance_left = jac_diff_left.T @ jac_diff_left
+
     # Get the Jacobian for the right foot.
     jac_right_foot = np.zeros((3, nv))
     mujoco.mj_jacBodyCom(
         model, data, jac_right_foot, None, model.body("right_knee_pitch_link").id
     )
-    # Calculate the QBalance matrix to keep both feet below the CoM.
-    jac_diff_left = jac_com - jac_left_foot
-    QbalanceL = jac_diff_left.T @ jac_diff_left
     jac_diff_right = jac_com - jac_right_foot
-    QbalanceR = jac_diff_right.T @ jac_diff_right
-    # Get DOFs for the root, joints, and balance
+    Qbalance_right = jac_diff_right.T @ jac_diff_right
+
+    # Get all joint names.
+    joint_names = [model.joint(i).name for i in range(model.njnt)]
+    # Get indices into relevant sets of joints.
     root_dofs = range(6)
     body_dofs = range(6, nv)
+    leg_dofs = [
+        model.joint(name).dofadr[0]
+        for name in joint_names
+        if ("hip" in name or "knee" in name)
+    ]
+    other_dofs = np.setdiff1d(body_dofs, leg_dofs)
     # Cost coefficients.
-    BALANCE_COST = 1e3  # Balancing.
-    JOINT_COST = 3  # Non-root joints.
+    BALANCE_COST = 1000  # Balancing.
+    LEG_JOINT_COST = 3  # Joints required for balancing.
+    OTHER_JOINT_COST = 0.3  # Other joints.
 
     # Construct the Qjoint matrix.
     Qjoint = np.eye(nv)
     Qjoint[root_dofs, root_dofs] *= 0  # Don't penalize free joint directly.
-    Qjoint[body_dofs, body_dofs] *= JOINT_COST
+    Qjoint[leg_dofs, leg_dofs] *= LEG_JOINT_COST
+    Qjoint[other_dofs, other_dofs] *= OTHER_JOINT_COST
     # Construct the Q matrix for position DoFs.
-    Qpos = BALANCE_COST * QbalanceL + BALANCE_COST * QbalanceR + Qjoint
-    # No explicit penalty for velocities.
+    Qpos = (
+        (BALANCE_COST / 2) * Qbalance_right
+        + (BALANCE_COST / 2) * Qbalance_left
+        + Qjoint
+    )
+    # No explicit penalty for velocitileft_knee_pitch_linkes.
     Q = np.block([[Qpos, np.zeros((nv, nv))], [np.zeros((nv, 2 * nv))]])
 
     # SIMULATE THE CONTROLLER
     # Constants
-    NUM_TRIALS = 1
+    NUM_TRIALS = 1000
     RENDER = True
-    RECALCULATE_A_B = False
     # Initialize score to 0
     score = 0
     for N in range(NUM_TRIALS):
         print("Trial", N)
-        INIT_A_B = False
         # Set the initial state and control.
         mujoco.mj_resetData(model, data)
-        data.ctrl = ctrl0
         data.qpos = qpos0
-        while True:  # data.qpos[2] > -0.6:
+        while data.time < 10:
             score += 1
-            # Allocate the A and B matrices, compute them.
             try:
-                if RECALCULATE_A_B or not INIT_A_B:
-                    A = np.zeros((2 * nv, 2 * nv))
-                    B = np.zeros((2 * nv, nu))
-                    epsilon = 1e-6
-                    flg_centered = True
-                    mujoco.mjd_transitionFD(
-                        model, data, epsilon, flg_centered, A, B, None, None
-                    )
-
-                    # Solve discrete Riccati equation.
-                    P = scipy.linalg.solve_discrete_are(A, B, Q, R)
-
-                    # Compute the feedback gain matrix K.
-                    K = np.linalg.inv(R + B.T @ P @ B) @ B.T @ P @ A
-                    # Allocate position difference dq.
-                    dq = np.zeros(model.nv)
-                    INIT_A_B = True
-                # Get state difference dx.
-                mujoco.mj_differentiatePos(model, dq, 1, qpos0, data.qpos)
-                dx = np.hstack((dq, data.qvel)).T
-                # LQR control law.
-                desired_torques = ctrl0 - K @ dx
-                positions = torques_to_positions(data.qpos, data.qvel, desired_torques)
-                data.ctrl = positions
-            except Exception as e:
-                print(e)
-                pass
+                start_comp_time = time.time()
+                desired_torques = control(model, data, Q, qpos0, ctrl0)
+                comp_time = time.time() - start_comp_time
+                print("Computation HZ: ", 1.0 / comp_time, end="\r")
+            except:
+                print(".                                            ")
+                desired_torques = ctrl0
+            data.ctrl = desired_torques
 
             mujoco.mj_step(model, data)
             if RENDER:
                 render()
-
-    print("score:", score)
-    print("RECALCULATE_A_B:", RECALCULATE_A_B)
