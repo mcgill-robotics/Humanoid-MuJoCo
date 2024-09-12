c2ee030dd9e1a381a4b3a5f5a845880837e37326
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-09-12 12:53:43 -0400

    clean up

---
+++ build.sh
@@ -0,0 +1,7 @@
+cd /Humanoid-MPC/mujoco_mpc && \
+    mkdir -p build && \
+    cd build && \
+    cmake .. -DCMAKE_BUILD_TYPE:STRING=Release -G Ninja -DCMAKE_C_COMPILER:STRING=clang-12 -DCMAKE_CXX_COMPILER:STRING=clang++-12 -DMJPC_BUILD_GRPC_SERVICE:BOOL=ON && \
+    cmake --build . --config=Release && \
+    cd /Humanoid-MPC/mujoco_mpc/python && \
+    python setup.py install
