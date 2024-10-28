d995c2187a4896005ed964defec7b8468e9b6202
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-10-28 16:59:53 -0400

    got position control lowkey working

--- controllers/mpc_server_pos_control.py
+++ controllers/mpc_server_pos_control.py
@@ -10,12 +10,10 @@ print("Server running...")
 
 mj_model, mj_data, renderer, JOINT_QPOS_IDX, JOINT_DOF_IDX, agent = get_mujoco_setup()
 
-PLANNER_HORIZON = 10
+PLANNER_HORIZON = 15
 DEBUG = True
 
-PREDICTED_POSITION_TIMESTEP = 1 / 50  # 50 Hz
-
-num_position_prediction_iterations = max(1, TIMESTEP // PREDICTED_POSITION_TIMESTEP)
+TORQUE_TO_POSITION_MULTIPLIER = 0.6
 
 while True:
     message = socket.recv_string()  # Receive a request
@@ -33,22 +31,16 @@ while True:
         state, mj_model, mj_data, JOINT_QPOS_IDX, JOINT_DOF_IDX
     )
     torque_ctrl = compute_action(agent, mj_data, PLANNER_HORIZON)
-    mj_model, mj_data = set_mujoco_state(
-        state, mj_model, mj_data, JOINT_QPOS_IDX, JOINT_DOF_IDX
-    )
-    mj_data.ctrl = torque_ctrl
-    for _ in num_position_prediction_iterations:
-        mujoco.mj_step(mj_model, mj_data)
-    joint_positions_after_control = mj_data.qpos[JOINT_QPOS_IDX]
+    
+    target_joint_positions = state[0] + TORQUE_TO_POSITION_MULTIPLIER * torque_ctrl
 
     # render(renderer, mj_data)
     socket.send_string(
-        json.dumps([float(x) for x in joint_positions_after_control])
+        json.dumps([float(x) for x in target_joint_positions])
     )  # Send the response back
     end_time = time.time()
     control_time = end_time - start_time
     print(f"Control freq.: {1.0 / control_time}")
-
     if DEBUG:
         # save image for debugging purposes
         frame = render(renderer, mj_data, display=False)
